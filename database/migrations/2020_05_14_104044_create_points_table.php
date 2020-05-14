9f6d30a0e3492fae3d6c66abfa8c959985648b6e
Gleb <zvgleb@yandex.ru>
2020-05-14 20:41:43 +0300

    fix

--- database/migrations/2020_05_14_104044_create_points_table.php
+++ database/migrations/2020_05_14_104044_create_points_table.php
@@ -16,8 +16,8 @@ class CreatePointsTable extends Migration
         Schema::create('points', function (Blueprint $table) {
             $table->integer('id')->unsigned()->unique();
             $table->integer('city_id');
-            $table->float('temperature')->nullable();
-            $table->float('wind_speed')->nullable();
+            $table->string('temperature', 10)->nullable();
+            $table->string('wind_speed', 10)->nullable();
             $table->string('condition_weather', 100)->nullable();
             $table->string('icon', 10)->nullable();
             $table->timestamps();
